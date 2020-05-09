1f0894fc508b916dcc9a884bc8ca614b95f298fa
jpd61 <jpd@dewoodyoil.com>
2020-05-09 14:18:12 -0500

    modular

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const fs = require('fs');
 
+const generatePage = require('./src/page-template.js');
 
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('============');
+const [name, github] = profileDataArgs;
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
