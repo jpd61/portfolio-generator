80309b7e969cc6373b3173be5cf384926e557fd3
jpd61 <jpd@dewoodyoil.com>
2020-05-09 15:14:21 -0500

    add when to input in userprompt

--- app.js
+++ app.js
@@ -43,10 +43,17 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
-        }
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => confirmAbout
+          }
     ]);
   };
 
