cdeadeea634d19bb5d18e7da87701ec66111e6ea
jpd61 <jpd@dewoodyoil.com>
2020-05-09 14:50:28 -0500

    installed inquirer

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
