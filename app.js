7018f88ae4367f53bf4946237908e9cc0fe1a0a2
jpd61 <jpd@dewoodyoil.com>
2020-05-09 13:39:06 -0500

    forEach is clutch

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+    console.log('============');
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+};
+
+printProfileData(profileDataArgs);
