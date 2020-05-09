d5575e43ef65d8e6cae4aee637b14f285a7b0f3c
jpd61 <jpd@dewoodyoil.com>
2020-05-09 16:19:40 -0500

    finalize

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const generatePage = require('./src/page-template.js');
 
@@ -131,11 +131,18 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
